fc13775ac0f133132f580041d8fbee0ba7215049
Spencer_Durfey <durfey_32@yahoo.com>
2024-10-13 14:52:34 -0600

    added front end queries and mutations

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,37 +1,107 @@
-// import { Query } from "mongoose";
-import type { BookDocument } from '../models/Book';
-import User from "../models/User";
-import { UserDocument } from "../models/User";
+import { BookDocument } from '../models/Book';
+import User, { UserDocument } from '../models/User';
+import { AuthenticationError, signToken } from '../utils/auth';
+
+interface AddUserArgs {
+    input: {
+        username: string;
+        email: string;
+        password: string;
+    };
+  }
+
+interface LoginUserArgs {
+    email: string;
+    password: string;
+}
+
+
+interface SaveBookArgs {
+    input: {
+      authors: string[];
+      description: string;
+      bookId: string;
+      image: string;
+      link: string;
+      title: string;
+    };
+  }
+  
+  interface RemoveBookArgs {
+    bookId: string;
+  }
 
 const resolvers = {
     Query: {
+        me: async (_parent: any, _args: any, context: { user: UserDocument | null }): Promise<UserDocument | null> => {
+            if (!context.user) {
+                throw new AuthenticationError('You must be logged in to view your profile');
+            }
+
+            const user = await User.findById(context.user._id).populate('savedBooks');
+            return user;
+        },
+
         bookSchema: async (_parent: any, { userId }: { userId: string }): Promise<BookDocument[] | null> => {
             const user = await User.findById(userId);
             return user ? user.savedBooks : null;
         },
-        
-        User: async (_parent: any, { _id }: { _id: string }): Promise<UserDocument[] | null> => {
-         const params = _id ? { _id } : {};
-            return User.find(params);
+
+        User: async (_parent: any, { _id }: { _id: string }): Promise<UserDocument | null> => {
+            return User.findById(_id).populate('savedBooks');
         },
     },
+
     Mutation: {
-        createUser: async (_parent: any, { username, email, password }: { username: string, email: string, password: string }): Promise<UserDocument> => {
-            return User.create({ username, email, password });
+        addUser: async (_parent: any, { input }: AddUserArgs): Promise<{ token: string; user: UserDocument }> => {
+            const user = await User.create(input);
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
         },
-        saveBook: async (_parent: any, { userId, bookId }: { userId: string, bookId: string }): Promise<UserDocument | null> => {
-            return User.findOneAndUpdate(
-                { _id: userId },
-                { $addToSet: { savedBooks: bookId } },
-                { new: true }
-            );
+
+        login: async (_parent: any, { email, password }: LoginUserArgs): Promise<{ token: string; user: UserDocument } | null> => {
+            const user = await User.findOne
+                ({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const isValidatePassword = await user.isCorrectPassword(password);
+            if (!isValidatePassword) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user.username, user.email, user._id);
+            return { token, user };
         },
-        deleteBook: async (_parent: any, { userId, bookId }: { userId: string, bookId: string }): Promise<UserDocument | null> => {
-            return User.findOneAndUpdate(
-                { _id: userId },
-                { $pull: { savedBooks: bookId } },
-                { new: true }
-            );
+
+        saveBook: async (_parent: any, { input }: SaveBookArgs, context: { user: UserDocument | null }): Promise<UserDocument | null> => {
+            if (!context.user) {
+              throw new AuthenticationError('You must be logged in to save a book');
+            }
+
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+        { $addToSet: { savedBooks: input } },
+        { new: true }
+      ).populate('savedBooks');
+
+      return updatedUser;
+    },
+
+    removeBook: async (_parent: any, { bookId }: RemoveBookArgs, context: { user: UserDocument | null }): Promise<UserDocument | null> => {
+        if (!context.user) {
+          throw new AuthenticationError('You must be logged in to remove a book');
+        }
+  
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        ).populate('savedBooks');
+  
+        return updatedUser;
         },
     },
 };
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,26 +1,47 @@
 const typeDefs = `
-type bookSchema {
+  type Book {
     authors: [String]
     description: String
     bookId: String
     image: String
     link: String
     title: String
-    }
+  }
 
 type User {
     _id: ID
     username: String
     email: String
     bookCount: Int
-    savedBooks: [bookSchema]
+    savedBooks: [Book]
+    }
+
+ type Auth {
+    token: String
+    user: User
+  }
+
+ input BookInput {
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+    title: String
+  }
+
+type Query {
+    me: User
     }
 
 type Mutation {
-    createUser(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+
     login(email: String!, password: String!): Auth
-    saveBook(userId: ID!, bookId: bookSchema!): User
-    deleteBook(userId: ID!, bookId: bookSchema!): User
+
+    saveBook(bookData: BookInput!): User
+
+    removeBook(bookId: String!): User
     }
 `;
 
