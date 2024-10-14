24fe9c57bd0b256ef78ae830d18167fb3f4fa997
Spencer_Durfey <durfey_32@yahoo.com>
2024-10-13 20:37:50 -0600

    attempting to debug

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,41 +1,45 @@
 import express from 'express';
 import path from 'node:path';
+import type { Request, Response } from 'express';
+import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
+import { authenticateToken } from './utils/auth.js';
 
-import { typeDefs, resolvers } from './schemas/index';
-import db from './config/connection.js';
-
-const app = express();
-const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 const startApolloServer = async () => {
   await server.start();
+  await db.openUri(process.env.MONGODB_URI || 'mongodb://localhost:27017/yourdbname');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  const PORT = process.env.PORT || 3001;
+  const app = express();
 
-app.use('/graphql', expressMiddleware(server));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
 
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
-}
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    app.get('*', (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
-app.listen(PORT, () => {
-  console.log(`API server running on prot ${PORT}!`);
-  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-});
-}
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+};
 
 startApolloServer();
