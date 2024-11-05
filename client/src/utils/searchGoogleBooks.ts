1b7163cc5560ac990b78cb2f54202ec3cd0b1c7b
Spencer_Durfey <durfey_32@yahoo.com>
2024-11-04 19:10:03 -0700

    api key not needed

--- client/src/utils/searchGoogleBooks.ts
+++ client/src/utils/searchGoogleBooks.ts
@@ -1,9 +1,10 @@
 // Google Books API search function
 
 export const searchGoogleBooks = (query: string) => {
-    const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
+    // const apiKey = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
     return fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`
+      `https://www.googleapis.com/books/v1/volumes?q=${query}`
+      // `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${apiKey}`
     );
   };
   
